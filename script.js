9f8ed37a70236a10468e1414536635da442b6150
akash-xo <stuakash13@gmail.com>
2026-03-27 00:40:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+setTimeout(()=>{
+
+document.querySelector(".intro").style.display="none"
+
+document.querySelector(".site").classList.add("show")
+
+document.body.style.overflow="auto"
+
+},3500)
+button.addEventListener("click", () => {
+  line.classList.add("animate");
+});
