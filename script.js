64ec666967095e38ba88fe53213807c15d9194f2
akash-xo <stuakash13@gmail.com>
2026-03-31 18:48:44 +0530

    Updated code

--- script.js
+++ script.js
@@ -5,8 +5,4 @@ document.querySelector(".intro").style.display="none"
 document.querySelector(".site").classList.add("show")
 
 document.body.style.overflow="auto"
-
-},3500)
-button.addEventListener("click", () => {
-  line.classList.add("animate");
-});
+},3500);
